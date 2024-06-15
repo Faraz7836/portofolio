e18628c00cc75c9472b847b96d71367e0b17ffd7
Faraz7836 <farazahmadiimt7836@gmail.com>
2024-06-15 20:44:23 +0530

    c

--- src/data/constants.js
+++ src/data/constants.js
@@ -198,9 +198,7 @@ export const experiences = [
 export const education = [
   {
     id: 0,
-    img: "https://www.facebook.com/photo/?fbid=639611481526346&set=a.639611388193022",
-    // "src/images/college logo.png",
-    //  "https://www.iimtindia.net/images/logo.png",
+    img: "https://www.iimtindia.net/images/logo.png",
     school: "IIMT COLLEGE OF ENGINEERING, GREATER NOIDA",
     date: "Apr 2019 - Apr 2023",
     grade: "73.2%",
